cfd9130ae587d29c07d62f346d495c4d5650c183
Pedro <pta2002@pta2002.com>
2017-02-21 20:29:53 +0000

    More PEP8

--- shop/cryptomethods.py
+++ shop/cryptomethods.py
@@ -2,38 +2,45 @@ from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 from django.conf import settings
 import bitaddress
 
+
 def get_rpc():
-	if not getattr(settings, 'TESTING', False):
-		return AuthServiceProxy("http://%s:%s@%s:%s" % (getattr(settings, 'RPC_USERNAME'),
-			getattr(settings, 'RPC_PASSWORD'),
-			getattr(settings, 'RPC_IP'),
-			getattr(settings, 'RPC_PORT')))
+    if not getattr(settings, 'TESTING', False):
+        return AuthServiceProxy("http://%s:%s@%s:%s" % (getattr(settings,
+                                                        'RPC_USERNAME'),
+                                getattr(settings, 'RPC_PASSWORD'),
+                                getattr(settings, 'RPC_IP'),
+                                getattr(settings, 'RPC_PORT')))
+
 
 def new_address():
-	if not getattr(settings, 'TESTING', False):
-		rpc = get_rpc()
-		return rpc.getnewaddress()
-	else:
-		return bitaddress.generate_address(version=0x37)
+    if not getattr(settings, 'TESTING', False):
+        rpc = get_rpc()
+        return rpc.getnewaddress()
+    else:
+        return bitaddress.generate_address(version=0x37)
+
 
 def getreceivedbyaddress(address, confirms):
-	if not getattr(settings, 'TESTING', False):
-		rpc = get_rpc()
-		return rpc.getreceivedbyaddress(address, confirms)
-	else:
-		return 0
+    if not getattr(settings, 'TESTING', False):
+        rpc = get_rpc()
+        return rpc.getreceivedbyaddress(address, confirms)
+    else:
+        return 0
+
 
 def settxfee(fee):
-	if not getattr(settings, 'TESTING', False):
-		rpc = get_rpc()
-		rpc.settxfee(fee)
+    if not getattr(settings, 'TESTING', False):
+        rpc = get_rpc()
+        rpc.settxfee(fee)
+
 
 def sendtoaddress(addr, ammount):
-	if not getattr(settings, 'TESTING', False):
-		rpc = get_rpc()
-		rpc.sendtoaddress(address, ammount)
+    if not getattr(settings, 'TESTING', False):
+        rpc = get_rpc()
+        rpc.sendtoaddress(address, ammount)
+
 
 def validateaddress(addr):
-	if getattr(settings, 'TESTING', False):
-		return get_rpc().validateaddress(request.GET['address'])
-	return len(addr) == 34 # TODO: More accurate
+    if getattr(settings, 'TESTING', False):
+        return get_rpc().validateaddress(request.GET['address'])
+    return len(addr) == 34  # TODO: More accurate
--- shop/decorators.py
+++ shop/decorators.py
@@ -4,24 +4,33 @@ from django.http import JsonResponse
 from functools import wraps
 from django.conf import settings
 
+
 def auth_required(forid):
-	def realone(func):
-		@wraps(func)
-		def _wrapped_view(request, *args, **kw):
-			if request.user.userextra.has_authorization(request, forid) or getattr(settings, 'TESTING', False):
-				return func(request, *args, **kw)
-			else:
-				return redirect(reverse('shop:authorize', kwargs={'forid':forid}) + '?next=' + request.path)
-		return _wrapped_view
-	return realone
+    def realone(func):
+        @wraps(func)
+        def _wrapped_view(request, *args, **kw):
+            if request.user.userextra.has_authorization(request, forid) or \
+             getattr(settings, 'TESTING', False):
+                return func(request, *args, **kw)
+            else:
+                return redirect(reverse('shop:authorize',
+                                        kwargs={'forid': forid})
+                                + '?next=' + request.path)
+        return _wrapped_view
+    return realone
+
 
 def auth_required_api(forid, goto=None):
-	def realone(func):
-		@wraps(func)
-		def _wrapped_view(request, *args, **kw):
-			if request.user.userextra.has_authorization(request, forid) or getattr(settings, 'TESTING', False):
-				return func(request, *args, **kw)
-			else:
-				return JsonResponse({'redirect':reverse('shop:authorize', kwargs={'forid':forid}) + '?next=' + goto or request.path})
-		return _wrapped_view
-	return realone
+    def realone(func):
+        @wraps(func)
+        def _wrapped_view(request, *args, **kw):
+            if request.user.userextra.has_authorization(request, forid) or \
+             getattr(settings, 'TESTING', False):
+                return func(request, *args, **kw)
+            else:
+                return JsonResponse({
+                    'redirect': reverse('shop:authorize',
+                                        kwargs={'forid': forid})
+                                    + '?next=' + goto or request.path})
+        return _wrapped_view
+    return realone
