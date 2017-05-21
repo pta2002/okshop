4e7372e755e42dc1f3308d180076a8ca275e9626
Pedro Alves <pta2002@pta2002.com>
2017-05-21 21:01:04 +0100

    Make forms horizontal

--- shop/forms.py
+++ shop/forms.py
@@ -28,4 +28,7 @@ class RegisterForm(forms.Form):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.add_input(Submit('submit', 'Register'))
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-8'
 
